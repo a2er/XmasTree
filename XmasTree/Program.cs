159d27c1ba8c5cf590d697d6c5e04895a58da304
thomasgalliker <thomas@bluewin.ch>
2025-12-21 11:56:24 +0100

    Add simple xmas tree

---
+++ XmasTree/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace XmasTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine(
+@"                 X
+                XXX
+              XXXXXXX
+            XXXXXXXXXXX
+              XXXXXXX
+            XXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXX
+      XXXXXXXXXXXXXXXXXXXXXXXXX
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+      XXXXXXXXXXXXXXXXXXXXXXXXX
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                #####
+                #####
+                #####
+
+           HAPPY CHRISTMAS");
+        }
+    }
+}
